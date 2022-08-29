2c6ade4ec5dd44255920486910d0ad5024a9e636
lucpes <lucpessoa01@gmail.com>
2022-08-28 22:34:37 -0300

    Trabalho da facul

--- recepcionista2.py
+++ recepcionista2.py
@@ -1,9 +1,14 @@
 from dataclasses import replace
 from datetime import datetime
 data = datetime.now()
-h_f = str(input('coloque a hora do filme (Exemplo: 15:30) '))
-h_f1 = h_f
-h_f = h_f.replace(":", "")
+
+while True:
+    h_f = str(input('coloque a hora do filme (Exemplo: 15:30) '))
+    h_f = h_f.replace(":", "")
+    if len(h_f) != 4:
+        print("Horário inválido")
+        continue
+    break
 
 h_at = (int(data.hour))
 m_at = (int(data.minute))
@@ -14,15 +19,17 @@ minuto = (h_f[2]) + (h_f[3])
 minuto = int(minuto)
 hora = hora*60
 resultado = hora + minuto
-print(resultado)
-print(h_atual)
 tempo = h_atual - resultado
 
-m_atrasado = m_at - minuto
+h_rest = h_at - int(h_f[0] + h_f[1])
+m_rest = m_at - int(h_f[2] + h_f[3])
+if m_rest < 0:
+    h_rest -= 1
+    m_rest += 60
 
 if tempo < -30:
-    print(f"Ainda são {h_at}:{m_at}, muito cedo para seu filme")
+    print(f"Ainda são {h_at}:{m_at}, muito cedo para seu filme.")
 elif tempo >= 30:
-    print(f"Voce está atrasado, sua sessão começou {h_f1}")
+    print(f"Voce está atrasado em {h_rest} hora(s) e {m_rest} minutos, as portas já fecharam.")
 else:
-    print("Pode entrar, primeira sala à direita")
+    print("Pode entrar, primeira sala à direita.")
